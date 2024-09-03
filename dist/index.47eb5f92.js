98b482970d2d7b0ca9527b5bf6f0e377b5200f98
SergeyPavlenko123 <sergey.pavlenko.1989@gmail.com>
2024-09-03 15:33:26 +0300

    new commit

--- dist/index.47eb5f92.js
+++ dist/index.47eb5f92.js
@@ -692,7 +692,7 @@ exports.default = moviesList = (arr)=>{
     let template = "";
     arr.forEach((element)=>{
         let { title, poster_path, genresNames, release_date, vote_average, id } = element;
-        template += `<li class="movie-list-card">
+        template += `<li class="movie-list-card" data-bs-toggle="modal" data-bs-target="#exampleModal">
                     <img class="movie-list-img" src="https://image.tmdb.org/t/p/w342${poster_path}" alt="movie picture" data-id=${id}>
                     <p class="movie-card-title">${title}</p>
                     <div class="card-list">
--- src/templates/moviesList.js
+++ src/templates/moviesList.js
@@ -4,7 +4,7 @@ export default moviesList = (arr) => {
   arr.forEach((element) => {
     let { title, poster_path, genresNames, release_date, vote_average, id } =
       element;
-    template += `<li class="movie-list-card">
+    template += `<li class="movie-list-card" data-bs-toggle="modal" data-bs-target="#exampleModal">
                     <img class="movie-list-img" src="https://image.tmdb.org/t/p/w342${poster_path}" alt="movie picture" data-id=${id}>
                     <p class="movie-card-title">${title}</p>
                     <div class="card-list">
